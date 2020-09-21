63d713170b480f69c1d6b5398500a5679c8fa7eb
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-09-21 20:07:20 +0100

    fix

--- {{cookiecutter.component_id}}/bin/code-gen.sh
+++ {{cookiecutter.component_id}}/bin/code-gen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/4.3.1/openapi-generator-cli-4.3.1.jar -O openapi-generator-cli.jar
+wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/4.3.1/openapi-generator-cli-4.3.1.jar -O /tmp/openapi-generator-cli.jar
 
-java -jar openapi-generator-cli.jar generate \
+java -jar /tmp/openapi-generator-cli.jar generate \
 -i {{cookiecutter.swagger_path}} \
 --api-package {{cookiecutter.api_package}} \
 --model-package {{cookiecutter.model_package}} \
@@ -11,6 +11,4 @@ java -jar openapi-generator-cli.jar generate \
 --artifact-id {{cookiecutter.component_id}} \
 --artifact-version {{cookiecutter.artifact_version}} \
 -g spring -p java8=true --library spring-cloud \
--o /
-
-rm -rf openapi-generator-cli.jar pom.xml
+-o .
